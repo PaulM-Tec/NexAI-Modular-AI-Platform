e4a05f44810fa16c7d083046e3af8ff5cdbf8ac2
Paul-Munhamo <pmunham@gmail.com>
2026-09-17 14:17:48 +0200

    Cleanup rep*sitory for public release

--- frontend/web/src/services/chat.ts
+++
@@ -1,5 +0,0 @@
-import { api } from './api';
- 
-export async function sendChatMessage(sessionId: number, userId: number, message: string) {
-   const res = await api.post('/chat', { session_id: sessionId, user_id: userId, message });
-  return res.data;
